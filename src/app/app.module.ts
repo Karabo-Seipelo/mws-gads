c007ae38bc2ad5554dbbb539c445222cfdc442d5
Karabo Seipelo <karabosouldier@gmail.com>
2019-10-17 19:57:41 +0200

    responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './home/app.component';
 
 import { MovieService } from './services/movie.service';
 
