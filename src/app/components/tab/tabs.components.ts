af06f4874321eeff88b5478fce60a27aa68afa42
Karabo Seipelo <karabosouldier@gmail.com>
2019-10-23 22:37:29 +0200

    cast and crew loading and styling

--- src/app/components/tab/tabs.components.ts
+++ src/app/components/tab/tabs.components.ts
@@ -2,53 +2,35 @@ import {
     Component,
     ContentChildren,
     QueryList,
-    AfterContentInit,
-    ViewChild,
-    ComponentFactoryResolver,
-    ViewContainerRef
+    AfterContentInit
   } from '@angular/core';
 
-  import { TabComponent } from './tab.component';
+import { TabComponent } from './tab.component';
 
-  @Component({
+@Component({
     selector: 'my-tabs',
-    template: `
-      <ul class="nav nav-tabs">
-        <li *ngFor="let tab of tabs" (click)="selectTab(tab)" [class.active]="tab.active">
-          {{tab.title}}
-        </li>
-      </ul>
-      <ng-content></ng-content>
-    `,
-    styles: [
-      `
-      .tab-close {
-        color: gray;
-        text-align: right;
-        cursor: pointer;
-      }
-      `
-    ]
+    templateUrl: './tabs.component.html',
+    styleUrls: ['./tabs.component.scss']
   })
   export class TabsComponent implements AfterContentInit {
-    
+
     @ContentChildren(TabComponent) tabs: QueryList<TabComponent>;
-    
+
     // contentChildren are set
     ngAfterContentInit() {
       // get all active tabs
-      let activeTabs = this.tabs.filter((tab)=>tab.active);
-      
+      const activeTabs = this.tabs.filter((tab) => tab.active);
+
       // if there is no active tab set, activate the first
-      if(activeTabs.length === 0) {
+      if (activeTabs.length === 0) {
         this.selectTab(this.tabs.first);
       }
     }
-    
-    selectTab(tab: Tab){
+
+    selectTab(tab: any) {
       // deactivate all tabs
       this.tabs.toArray().forEach(tab => tab.active = false);
-      
+
       // activate the tab the user has clicked on.
       tab.active = true;
     }
