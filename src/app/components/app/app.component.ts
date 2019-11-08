fa0e4407d63b4696cdd4342194fbc2d006ae0927
Karabo Seipelo <Karabo.Seipelo@wintechnologies.net>
2019-11-08 16:27:45 +0200

    mobile menu

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent  implements OnInit {
   title = 'mws-gads';
+  isOpen = false;
 
   constructor(
   ) {}
+
+  toggleMenu = (toggle) => {
+    //this.isOpen = !this.isOpen;
+    this.isOpen = !toggle;
+  }
 
   ngOnInit() {
   }
